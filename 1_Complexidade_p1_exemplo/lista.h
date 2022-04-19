1694c256ff0454ddd31eaf9b5f385e5b1f5db08c
lepenidon <penido1000@gmail.com>
2022-04-19 14:59:19 -0300

    arquivos c_01

---
+++ 1_Complexidade_p1_exemplo/lista.h
@@ -0,0 +1,17 @@
+#define TAM 10000000
+
+typedef int elem;
+
+typedef struct {
+	elem *dados;
+    elem *copia;
+	long tamanho;
+} lista;
+
+void criar(lista *l);
+void destruir(lista *l);
+int inserir(lista *l, elem x);
+void imprimir(lista l);
+void ordenar_n2(lista l);
+void ordenar_nlogn(lista l);
+ 
