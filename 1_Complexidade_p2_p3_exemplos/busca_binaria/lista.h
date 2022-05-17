76afb83428596c7d3aa299d97efa45e6b2199399
lepenidon <penido1000@gmail.com>
2022-05-17 10:24:43 -0300

    estudos

--- 1_Complexidade_p2_p3_exemplos/busca_binaria/lista.h
+++ 1_Complexidade_p2_p3_exemplos/busca_binaria/lista.h
@@ -1,11 +1,11 @@
-#define TAM 1000000000
+#define TAM 10
 
 typedef int elem;
 
 typedef struct {
-	elem *dados;
+    elem *dados;
     elem *copia;
-	long tamanho;
+    long tamanho;
 } lista;
 
 void criar(lista *l);
@@ -14,3 +14,5 @@ int inserir(lista *l, elem x);
 void imprimir(lista l);
 int buscar(lista l, elem x);
 void ordenar_nlogn(lista l);
+int buscar_recursivo_penido(lista l, elem x, long ini, long fim);
+int buscar_penido(lista l, elem x);
--- 1_Complexidade_p2_p3_exemplos/exponenciacao/main.c
+++ 1_Complexidade_p2_p3_exemplos/exponenciacao/main.c
@@ -10,21 +10,21 @@ long exponenciacao_n(long m, long n) {
 long exponenciacao_logn(long m, long n) {
     // m^n = (m * m)^(n/2) , se n eh par
     // m^n = ((m * M)^(n/2)) * m, se n eh impar
-    
+
     // Recorrencia
     // T(0) = c
     // T(n) = c + T(n / 2), para n > 0
-    
+
     if (n == 0)
-        return 1; // caso trivial
+        return 1;  // caso trivial
     if (n % 2 == 0)
-        return exponenciacao_logn(m * m, n / 2); // n eh par
-    return exponenciacao_logn(m * m, n /2) * m; // n em impar
+        return exponenciacao_logn(m * m, n / 2);  // n eh par
+    return exponenciacao_logn(m * m, n / 2) * m;  // n em impar
 }
 
 int main(void) {
-    long m = 2, n = 4;
-    
+    long m = 2, n = 5;
+
     printf("%ld elevado a %ld eh: %ld\n", m, n, exponenciacao_logn(m, n));
     return 0;
 }
